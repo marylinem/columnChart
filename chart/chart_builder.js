6c7d51bd38d156f84cd6cc0211080212efc6f13d
Mengoualeu Majiade <mame1@MN-EX1377.local>
2022-07-12 17:31:55 +0200

    code modified

--- chart/chart_builder.js
+++ chart/chart_builder.js
@@ -1,181 +1,55 @@
-   (function() { 
-	let template = document.createElement("template");
-	template.innerHTML = `
-		<style>		
-		:host #chartdiv {
-			width: 100%;
-			height: 500px;
-			border-color: #92a8d1;
-			border-style: solid;
+(function () {
+    let template = document.createElement("template");
+    template.innerHTML = `
+    <form id="form">
+    <fieldset>
+    <legend>Colored Box Properties</legend>
+    <table>
+    <tr>
+    <td>Opacity</td>
+    <td><input id="builder_opacity" type="text" size="5"
+   maxlength="5"></td>
+    </tr>
+    </table>
+    <input type="submit" style="display:none;">
+    </fieldset>
+    </form>
+    <style>
+    :host {
+    display: block;
+    padding: 1em 1em 1em 1em;
+    }
+    </style>
+    `;
+    class ChartBuilderPanel extends HTMLElement {
+        constructor() {
+            super();
+            this._shadowRoot = this.attachShadow({ mode: "open" });
+            this._shadowRoot.appendChild(template.content.cloneNode(true));
 
-			}
-		</style>
-
-		<div id="chartdiv"></div>
-
-	`;
-
-	class ChartBuilderPanel extends HTMLElement {
-		constructor() {
-			super(); 
-			let shadowRoot = this.attachShadow({mode: "open"});
-			shadowRoot.appendChild(template.content.cloneNode(true));	
-			this._props = {};
-			this.init(shadowRoot)
-		}
-
-
-		async init(shadowRoot){
-			for(let fileName of ["index.js","xy.js","themes/Animated.js"]){
-				await load(fileName)
-				//script.onload = function(){ customElements.define("com-demo-chart", Chart);}; 
-		
-			}
-			console.log(document,shadowRoot.querySelector("#chartdiv"))
-			chart(shadowRoot);
-
-		}
-	}
-	customElements.define("com-demo-chartBuilderPanel", ChartBuilderPanel);
-
-	
-
-
-	function load(fileName){
-	return new Promise(
-		function(resolve,reject){
-			var script = document.createElement("script"); 
-			script.type = "text/javascript"; 
-			script.src = "https://cdn.amcharts.com/lib/5/"+fileName; 
-			script.onload =  resolve; 
-			script.onerror = reject
-			document.head.appendChild(script);	
-			console.log(script);
-		}
-	)
-	
-	}
-
-
-	function chart(shadowRoot){
-
-		am5.ready(function() {
-
-		// Create root element
-		var root = am5.Root.new(shadowRoot.querySelector("#chartdiv"));
-
-		// Set themes
-		root.setThemes([
-		am5themes_Animated.new(root)
-		]);
-
-		// Create chart
-		var chart = root.container.children.push(am5xy.XYChart.new(root, {
-		panX: true,
-		panY: true,
-		wheelX: "panX",
-		wheelY: "zoomX",
-		pinchZoomX:true
-		}));
-
-		// Add cursor
-		var cursor = chart.set("cursor", am5xy.XYCursor.new(root, {}));
-		cursor.lineY.set("visible", false);
-
-
-		// Create axes
-		var xRenderer = am5xy.AxisRendererX.new(root, { minGridDistance: 30 });
-		xRenderer.labels.template.setAll({
-		rotation: -90,
-		centerY: am5.p50,
-		centerX: am5.p100,
-		paddingRight: 15
-		});
-
-		var xAxis = chart.xAxes.push(am5xy.CategoryAxis.new(root, {
-		maxDeviation: 0.3,
-		categoryField: "country",
-		renderer: xRenderer,
-		tooltip: am5.Tooltip.new(root, {})
-		}));
-
-		var yAxis = chart.yAxes.push(am5xy.ValueAxis.new(root, {
-		maxDeviation: 0.3,
-		renderer: am5xy.AxisRendererY.new(root, {})
-		}));
-
-
-		// Create series
-		var series = chart.series.push(am5xy.ColumnSeries.new(root, {
-		name: "Series 1",
-		xAxis: xAxis,
-		yAxis: yAxis,
-		valueYField: "value",
-		sequencedInterpolation: true,
-		categoryXField: "country",
-		tooltip: am5.Tooltip.new(root, {
-			labelText:"{valueY}"
-		})
-		}));
-
-		series.columns.template.setAll({ cornerRadiusTL: 5, cornerRadiusTR: 5 });
-		series.columns.template.adapters.add("fill", function(fill, target) {
-		return chart.get("colors").getIndex(series.columns.indexOf(target));
-		});
-
-		series.columns.template.adapters.add("stroke", function(stroke, target) {
-		return chart.get("colors").getIndex(series.columns.indexOf(target));
-		});
-
-
-		// Set data
-		var data = [{
-		country: "USA",
-		value: 2025
-		}, {
-		country: "China",
-		value: 1882
-		}, {
-		country: "Japan",
-		value: 1809
-		}, {
-		country: "Germany",
-		value: 1322
-		}, {
-		country: "UK",
-		value: 1122
-		}, {
-		country: "France",
-		value: 1114
-		}, {
-		country: "India",
-		value: 984
-		}, {
-		country: "Spain",
-		value: 711
-		}, {
-		country: "Netherlands",
-		value: 665
-		}, {
-		country: "Russia",
-		value: 580
-		}, {
-		country: "South Korea",
-		value: 443
-		}, {
-		country: "Canada",
-		value: 441
-		}];
-
-		xAxis.data.setAll(data);
-		series.data.setAll(data);
-
-
-		// Make stuff animate on load
-		series.appear(1000);
-		chart.appear(1000, 100);
-
-		}); // end am5.ready()
-	}
-	
+            this._shadowRoot.getElementById("form").addEventListener("submit",
+                this._submit.bind(this));
+        }
+        _submit(e) {
+            e.preventDefault();
+            this.dispatchEvent(new CustomEvent("propertiesChanged", {
+                detail: {
+                    properties: {
+                        opacity: this.opacity
+                    }
+                }
+            }));
+        }
+        set opacity(newOpacity) {
+            this._shadowRoot.getElementById("builder_opacity").value =
+                newOpacity;
+        }
+        get opacity() {
+            return this._shadowRoot.getElementById("builder_opacity").value;
+        }
+    }
+    customElements.define("com-demo-chartBuilderPanel", ChartBuilderPanel);
 })();
+
+
+
